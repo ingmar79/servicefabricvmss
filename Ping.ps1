35a7eceb7b15556e0793bd8b9a37842b71f1b1c9
Gonzalo <me@gonzaloruiz.co.uk>
2016-02-25 16:23:03 +0000

    Inital Commit

---
+++ Ping.ps1
@@ -0,0 +1,21 @@
+﻿
+
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+    $serviceUrl = "https://2a1ecc99-13fd-46e2-a3c8-9e59917e5717.cloudapp.net/resources/"
+    #$serviceUrl ="http://10.0.2.5:5000/activities/"
+    while($true) { 
+        Invoke-WebRequest -Uri  $serviceUrl |Select -ExpandProperty RawContent
+        Start-Sleep -Milliseconds 1000 
+        }
